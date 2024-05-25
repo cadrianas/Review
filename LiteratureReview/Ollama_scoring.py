a80e323919e11272c5ed477368cc83e559f7fd64
Adriana <cadrianas@gmail.com>
2024-05-25 15:03:08 -0500

    typo fixed

--- LiteratureReview/Ollama_scoring.py
+++ LiteratureReview/Ollama_scoring.py
@@ -113,7 +113,7 @@ def grade_articles(input_csv, output_csv):
             ])
 
 if __name__ == "__main__":
-    input_csv = "outputs/combined_outputs_ollama.csv"
+    input_csv = "outputs/combined_output_ollama.csv"
     output_csv = "graded_outputs_ollama.csv"
     grade_articles(input_csv, output_csv)
 
